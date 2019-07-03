d57d95af2fa8361a9fdb41a96edfe521a8dac737
tugba.kaya <tugba.kaya@atos.net>
2019-07-03 14:41:17 +0300

    Builder pattern is added

--- builder-pattern-person/src/main/java/com/tugbakaya/builderpatternperson/BuilderPatternPersonApplication.java
+++ builder-pattern-person/src/main/java/com/tugbakaya/builderpatternperson/BuilderPatternPersonApplication.java
@@ -5,8 +5,8 @@ import com.tugbakaya.builderpatternperson.person.Person;
 public class BuilderPatternPersonApplication {
 	public static void main(String[] args) {
 		System.out.println("Builder pattern person application");
-		
 		Person person = new Person.Builder().setName("jack").setSurname("johnson").setAge(5).getPerson();
+		
 		System.out.println(person.getName() +" "+ person.getSurname()+" " + person.getAge());
 
 	}
